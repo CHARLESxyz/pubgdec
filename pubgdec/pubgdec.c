eb2949a58cadfb5d7730fa13c3e3630f18951c5d
nmodnation <nmodnation@gmail.com>
2018-01-14 03:44:05 +0900

    pubgdec 1.0

--- pubgdec/pubgdec.c
+++ pubgdec/pubgdec.c
@@ -1,64 +1,73 @@
-#include <stdarg.h>
-#include <stdio.h>
-#include "def.h"
+/*
+pubgdec 1.0
+*/
 
 // https://github.com/calccrypto/uint128_t
-#ifdef __cplusplus
+#if !defined(__GNUC__) && defined(__cplusplus)
 #include "uint128_t.h"
 #endif
 
-#ifdef _MSC_VER
+typedef unsigned int uint;
+typedef unsigned char uchar;
+typedef unsigned short ushort;
+typedef unsigned long ulong;
 
+typedef char int8;
+typedef unsigned char uint8;
+typedef short int16;
+typedef unsigned short uint16;
+typedef int int32;
+typedef unsigned int uint32;
+typedef long long int64;
+typedef unsigned long long uint64;
+
+#define _BYTE uint8
+#define _WORD uint16
+#define _DWORD uint32
+#define _QWORD uint64
+
+#define LAST_IND(x, part_type) (sizeof(x) / sizeof(part_type) - 1)
+#define HIGH_IND(x, part_type) LAST_IND(x, part_type)
+#define LOW_IND(x, part_type) 0
+
+#define BYTEn(x, n)   (*((_BYTE*)&(x) + n))
+#define WORDn(x, n)   (*((_WORD*)&(x) + n))
+#define DWORDn(x, n)  (*((_DWORD*)&(x) + n))
+
+#define LOBYTE(x) BYTEn(x, LOW_IND(x, _BYTE))
+#define LOWORD(x) WORDn(x, LOW_IND(x, _WORD))
+#define LODWORD(x) DWORDn(x, LOW_IND(x, _DWORD))
+#define HIBYTE(x) BYTEn(x, HIGH_IND(x, _BYTE))
+#define HIWORD(x) WORDn(x, HIGH_IND(x, _WORD))
+#define HIDWORD(x) DWORDn(x, HIGH_IND(x, _DWORD))
+#define BYTE1(x) BYTEn(x, 1)
+#define BYTE2(x) BYTEn(x, 2)
+#define BYTE3(x) BYTEn(x, 3)
+#define BYTE4(x) BYTEn(x, 4)
+#define BYTE5(x) BYTEn(x, 5)
+#define BYTE6(x) BYTEn(x, 6)
+#define BYTE7(x) BYTEn(x, 7)
+#define BYTE8(x) BYTEn(x, 8)
+#define BYTE9(x) BYTEn(x, 9)
+#define BYTE10(x) BYTEn(x, 10)
+#define BYTE11(x) BYTEn(x, 11)
+#define BYTE12(x) BYTEn(x, 12)
+#define BYTE13(x) BYTEn(x, 13)
+#define BYTE14(x) BYTEn(x, 14)
+#define BYTE15(x) BYTEn(x, 15)
+#define WORD1(x) WORDn(x, 1)
+#define WORD2(x) WORDn(x, 2)
+#define WORD3(x) WORDn(x, 3)
+#define WORD4(x) WORDn(x, 4)
+#define WORD5(x) WORDn(x, 5)
+#define WORD6(x) WORDn(x, 6)
+#define WORD7(x) WORDn(x, 7)
+#define DWORD1(x) DWORDn(x, 1)
+
+#ifdef _MSC_VER
 #pragma warning(disable: 4244)
 #pragma warning(disable: 4293)
-
 #endif
-
-#ifdef __cplusplus
-#define EXTERN extern "C"
-#else
-#define EXTERN
-#endif
-
-/*
-InterlockedCompareExchange128
-*/
-
-#ifdef __GNUC__
-
-static unsigned char _InterlockedCompareExchange128(int64 volatile *Destination, int64 ExchangeHigh, int64 ExchangeLow, int64* ComparandResult) {
-	unsigned char Equal;
-	__asm__ __volatile__
-	(
-		"lock cmpxchg16b %1\n\t"
-		"setz %0"
-		: "=q" (Equal), "+m" (*Destination), "+d" (ComparandResult[1]), "+a" (ComparandResult[0])
-		: "c" (ExchangeHigh), "b" (ExchangeLow)
-		: "cc"
-	);
-	return Equal;
-}
-
-#else
-
-EXTERN unsigned char __cdecl _InterlockedCompareExchange128(int64 volatile *Destination, int64 ExchangeHigh, int64 ExchangeLow, int64 *ComparandResult);
-
-#endif
-
-/*
-debug
-*/
-
-EXTERN int __stdcall MessageBoxA(void *, const char *, const char *, int);
-
-void debug(const char *format, ...) {
-	va_list ap;
-	va_start(ap, format);
-	char text[0x200];
-	vsprintf(text, format, ap);
-	va_end(ap);
-	MessageBoxA((void *)0, text, "debug", 0);
-}
 
 /*
 rpm
@@ -85,7 +94,10 @@ typedef struct dummy_request {
 	uint64 size;
 } dummy_request;
 
-EXTERN int __stdcall WriteFile(void *, const void *, uint, uint *, void *);
+#ifdef __cplusplus
+extern "C"
+#endif
+int __stdcall WriteFile(void *, const void *, uint, uint *, void *);
 
 static int dummy_read(dummy *dummy, void *game_addr, void *user_addr, uint64 size) {
 	dummy_request request;
@@ -123,6 +135,8 @@ static uint64 dummy_read64(dummy *dummy, void *game_addr) {
 	return ret;
 }
 
+#define GET_ADDR(addr) (dummy->game_base_addr + (addr))
+
 #define READ(addr, dest, size) dummy_read(dummy, (void *)(addr), dest, size)
 #define READ8(addr) dummy_read8(dummy, (void *)(addr))
 #define READ16(addr) dummy_read16(dummy, (void *)(addr))
@@ -138,19 +152,17 @@ typedef struct int128 {
 	int64 high;
 } int128;
 
-#ifdef __cplusplus
-typedef uint128_t uint128;
-#else
 #ifdef __GNUC__
 typedef unsigned __int128 uint128;
+#else
+#ifdef __cplusplus
+typedef uint128_t uint128;
 #else
 #error uint128
 #endif
 #endif
 
-#define GET_ADDR(addr) (dummy->game_base_addr + (addr))
-
-#define TABLE_SIZE 256
+#define TABLE_SIZE 0x100
 
 typedef struct decrypt_struct {
 	uint8 table[TABLE_SIZE];
@@ -160,6 +172,7 @@ typedef struct decrypt_struct {
 
 static decrypt_struct g_decrypt;
 
+// export
 void decinit(dummy *dummy) {
 	READ(GET_ADDR(0x3AFF120), g_decrypt.table, TABLE_SIZE);
 	g_decrypt.xor1 = READ32(700 + GET_ADDR(0x3DEB690));
@@ -167,16 +180,11 @@ void decinit(dummy *dummy) {
 }
 
 static uint8 read_table(uint32 index) {
-	if (index >= TABLE_SIZE) {
-		debug("[read_table] out of bounds = %d", index);
-	}
 	return g_decrypt.table[index];
 }
 
-static uint64 dec1(dummy *dummy, int128 *_RCX22) {
+static uint64 dec1(dummy *dummy, int128 _RCX22) {
 	uint64 v1;
-	int128 v2;
-	uint8 v3;
 	uint v4;
 	int16 v5;
 	uint v6;
@@ -188,15 +196,10 @@ static uint64 dec1(dummy *dummy, int128 *_RCX22) {
 	uint v14;
 	char v16;
 	int v19;
-	do
-	{
-		v2 = *_RCX22;
-		v3 = _InterlockedCompareExchange128((int64 *)_RCX22, v2.high, v2.low, (int64 *)&v2);
-		v1 = v2.high;
-		v19 = DWORD1(v2);
-		v4 = v2.low;
-		v5 = WORD4(v2);
-	} while (!v3);
+	v1 = _RCX22.high;
+	v19 = DWORD1(_RCX22);
+	v4 = _RCX22.low;
+	v5 = WORD4(_RCX22);
 	v6 = 2067041945;
 	v7 = ((_DWORD)v1
 		+ v4
@@ -218,7 +221,6 @@ static uint64 dec1(dummy *dummy, int128 *_RCX22) {
 	} while (v9 < 3);
 	if ((v6 ^ (uint)v7) != v19)
 	{
-		// error
 		return 0;
 	}
 	v13 = v5 ^ ~(_WORD)v4 ^ 0xD25;
@@ -234,9 +236,8 @@ static uint64 dec1(dummy *dummy, int128 *_RCX22) {
 		(READ32((uint64)(4) * (uint8)(HIBYTE(v13) ^ 0x5A) + GET_ADDR(0x3DEBA90)) ^ g_decrypt.xor1)) % 0x2B;
 }
 
-static uint64 dec2(dummy *dummy, int128 *_RCX23) {
+static uint64 dec2(dummy *dummy, int128 _RCX23) {
 	uint64 v1;
-	int128 v2;
 	uint8 v3;
 	uint v4;
 	int16 v5;
@@ -249,15 +250,10 @@ static uint64 dec2(dummy *dummy, int128 *_RCX23) {
 	uint v14;
 	char v16;
 	int v19;
-	do
-	{
-		v2 = *_RCX23;
-		v3 = _InterlockedCompareExchange128((int64 *)_RCX23, v2.high, v2.low, (int64 *)&v2);
-		v1 = v2.high;
-		v19 = DWORD1(v2);
-		v4 = v2.low;
-		v5 = WORD4(v2);
-	} while (!v3);
+	v1 = _RCX23.high;
+	v19 = DWORD1(_RCX23);
+	v4 = _RCX23.low;
+	v5 = WORD4(_RCX23);
 	v6 = 2067041945;
 	v7 = ((_DWORD)v1
 		+ v4
@@ -279,7 +275,6 @@ static uint64 dec2(dummy *dummy, int128 *_RCX23) {
 	} while (v9 < 3);
 	if ((v6 ^ (uint)v7) != v19)
 	{
-		// error
 		return 0;
 	}
 	v13 = v5 ^ ~(_WORD)v4 ^ 0xD25;
@@ -295,13 +290,13 @@ static uint64 dec2(dummy *dummy, int128 *_RCX23) {
 		(READ32((uint64)(4) * (uint8)(HIBYTE(v13) ^ 0xE3) + GET_ADDR(0x3DEBA90)) ^ g_decrypt.xor2)) % 0x2B;
 }
 
-volatile int128 rcx22;
-volatile int128 rcx23;
-
+// export
 uint64 decptr(dummy *dummy, void *x) {
+	int128 rcx22;
+	int128 rcx23;
 	READ((int128 *)x + 22, &rcx22, sizeof(int128));
 	READ((int128 *)x + 23, &rcx23, sizeof(int128));
-	uint64 xor1 = READ64((uint64 *)x + dec1(dummy, &rcx22));
-	uint64 xor2 = dec2(dummy, &rcx23);
+	uint64 xor1 = READ64((uint64 *)x + dec1(dummy, rcx22));
+	uint64 xor2 = dec2(dummy, rcx23);
 	return xor1 ^ xor2;
 }
